407d3efe2f6b4858c9ebb168b95d4ce3f7f1edbf
unknown <jnucsetrisha99@gmail.com>
2023-09-09 15:05:51 +0600

    verifying error

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,91 @@
 "use client"
-import { useEffect,useState } from "react";
-import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import Header from "./Components/Header";
+import { ethers } from "ethers";
 import contractabi from "./abi/abi.json";
-import {ethers} from "ethers";
+const axios = require('axios');
+const FormData = require('form-data') ;
 
 export default function Home() {
-  const [address,setAddress] =useState(null);
-  const [balance,setBalance] =useState(0);
-  const [contract,setContract] =useState(null);
-  
-
+  const [address, setAddress] = useState(null);
+  const [balance, setBalance] = useState(0);
+  const [contract, setContract] = useState(null);
+  const [name, setName] = useState("");
+  const [description,setDescription] = useState("");
+  const [image, setImage] = useState(null);  
   useEffect(()=>{
     async function initialize(){
-      if(typeof window.ethereum!=="undefined"){
-        const provider= new ethers.providers.Web3Provider(window.ethereum)
-        const signer=provider.getSigner();
+      if(typeof window.ethereum !== "undefined"){
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
         const address = await signer.getAddress();
-        const balance=await provider.getBalance(address);
+        const balance = await provider.getBalance(address);
         setAddress(address);
-        setBalance(ethers.utils.parseEther(balance))
-        mycontractaddress="0xa147741D864f065c3704D1875Fa4b7732624B834"
-        const contract = new ethers.contract(mycontractaddress,contractabi,signer)
+        // setBalance(ethers.utils.parseEther(balance));
+        const mycontractaddress="0xDF2Bba0535EAb94Fa772398555554d143505ee22";
+        const contract = new ethers.Contract(mycontractaddress,contractabi,signer)
         setContract(contract)
+      }
+    }
+    initialize();
+  },[])
+
+  function onChangeFile(e){
+    const file = e.target.files[0];
+    setImage(file);
+    console.log(file);
+  }
+
+  async function onSubmit(event){
+    if(!name&&!description&&!image){
+      alert("Fill the required details");
+      return;
+    }
+    const JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI3YTcwYzVjOS00ZTVkLTQwZmEtOTkyZi04M2E3YThjOTFhNjgiLCJlbWFpbCI6ImpudWNzZXRyaXNoYTk5QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIxYTQ0YzYzNDUwZGE2NTA2ZDdjMCIsInNjb3BlZEtleVNlY3JldCI6ImIzNjA5NjIwYzNlZTRmYmM0NTNiZjFiMmUzYWZhM2Q2NmNhY2I2ZjYzNDg5ODA0YTJjZjhiMmI4NGUzZTYzZjYiLCJpYXQiOjE2OTQyNDQzMjF9.gR9VX15Fx8K8sE85xgO9QS-eUts7xY-thJrSJa0urY4";
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', image);
+    const pinataMetadata = JSON.stringify({
+      name: 'File name',
+    });
+    formData.append('pinataMetadata', pinataMetadata);
+    
+    const pinataOptions = JSON.stringify({
+      cidVersion: 0,
+    })
+    formData.append('pinataOptions', pinataOptions);
+
+    try{
+      const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+        maxBodyLength: "Infinity",
+        headers: {
+          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+          Authorization:"Bearer" + JWT,
+        }
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
     }
   }
-  initialize();
-  },[])
-  
-
   return (
     <>
     <div>
-     <Header/>
-     <div className="text-center">
-     <p className=" text-md text-blue-400 lg:text-3xl">Hi,{address?.slice(0,10)}...{address?.slice(-10)}
-     </p>
-     </div>
+   <Header/>
+   <div className="text-center">
+   <p className="text-md text-blue-400 lg:text-3xl">Hi, {address?.slice(0,10)}...{address?.slice(-10)} </p>
+   <div className="flex bg-yellow-400 px-10 mt-5 flex-col space-y-4 py-10 rounded-xl md:mx lg:mx-[400px]">
+   <p>NFT MarketPlace</p>
+   <input type="text" placeholder="Enter your name" value={name} onChange={(e)=>{setName(e.target.value)}} className="border border-black px-2 " />
+   <input type="text" className="h-20" placeholder="Enter your description" value={description} onChange={(e)=>{setDescription(e.target.value)}} className="border border-black px-2 " />
+   <div>
+   <label>Upload Image</label>
+   <input type="file" className="mt-2" accept="image/*" onChange={onChangeFile} />
+   <button className="bg-blue-400 px-4 py-2 rounded-lg mt-4" onClick={onSubmit}>Submit</button>
+   </div>
+   </div>
+  </div>
     </div>
     </>
-  )}
+  )
+}
